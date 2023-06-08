809494f3a770f20b6e5fece2a1a5d5266f1420dc
Thiago Carvalho <thpc14@gmail.com>
2023-06-07 23:46:02 -0300

    changes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {    
+        short numeroCurto = 1;
+        short numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
